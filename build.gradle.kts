d54df5532dce6c9913e1f299e8aad331d8e21830
Vova Bilous <vova.bilous@hotmail.com>
2020-09-21 19:08:49 +0300

    enable jacoco reports
    Publicate project on GitHub to start "open source" collaboration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "1.4.0"
     id("org.jetbrains.intellij") version "0.4.12" apply false
     id("org.jetbrains.gradle.plugin.idea-ext") version "0.7" apply false
+	jacoco
 }
 
 group = "pro.bilous"
@@ -9,6 +10,7 @@ version = "1.0-SNAPSHOT"
 
 allprojects {
     repositories {
+		mavenLocal()
         mavenCentral()
         jcenter()
     }
@@ -34,4 +36,17 @@ tasks {
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
+}
+
+tasks.withType<JacocoReport> {
+	executionData(
+		fileTree(project.rootDir.absolutePath).include("**/build/jacoco/*.exec")
+	)
+
+	reports {
+		xml.isEnabled = true
+		xml.destination = file("${buildDir}/reports/jacoco/report.xml")
+		html.isEnabled = false
+		csv.isEnabled = false
+	}
 }
