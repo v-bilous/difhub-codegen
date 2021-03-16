94e25e395842e1e53407a214b5ba798da9868afc
Constantine Zubenko <constantinefoo@gmail.com>
2021-03-16 10:26:06 +0200

    more sql keywords added to escape list

--- codegen-cli/src/main/java/pro/bilous/codegen/utils/SqlNamingUtils.kt
+++ codegen-cli/src/main/java/pro/bilous/codegen/utils/SqlNamingUtils.kt
@@ -2,7 +2,8 @@ package pro.bilous.codegen.utils
 
 object SqlNamingUtils {
 
-	private val sqlReservedWordsToEscape = arrayOf("use", "open", "drop", "create", "table", "rank", "system", "function", "range", "from", "order", "condition")
+	private val sqlReservedWordsToEscape = arrayOf("use", "open", "drop", "create", "table", "rank", "system", "function",
+		"range", "from", "order", "condition", "limit", "procedure", "check", "index", "column", "database")
 
 	fun escapeSqlNameIfNeeded(tableName: String): String {
 		return if (sqlReservedWordsToEscape.contains(tableName)) "${tableName}_" else tableName
